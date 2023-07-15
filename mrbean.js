fd3c0694d7d0caff0daa4fcfd3e7cf81e13d3cf4
Rakesh <72788505+name356@users.noreply.github.com>
2023-07-15 03:48:56 -0700

    Add files via upload

---
+++ mrbean.js
@@ -0,0 +1,12 @@
+var  eye = document.getElementsByClassName("blackeye");
+document.onmousemove = function(){
+var width = window.innerWidth;
+var x = event.clientX;
+var a = x / width;
+a= a *100;
+var height = window.innerHeight;
+var y = event.clientY;
+var b = y / height;
+b= b *100;
+eye[0].style.cssText = "left: " + a + "%; top: " + b + "%;" + "transform:translate(-"+a+"%,-"+b+"%)";
+eye[1].style.cssText = "left: " + a + "%; top: " + b + "%;" + "transform:translate(-"+a+"%,-"+b+"%)"; }
